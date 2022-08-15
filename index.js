d2ee29c37346562476b051a61846e0c212e25096
Felix <barosiofelix@gmail.com>
2022-08-15 20:53:16 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+
+let random;
+
+random = Math.floor(Math.random()*10);
+
+
+let num3 = 9;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+
+let max;
+
+max = Math.max(5, 10, 15, 20)
